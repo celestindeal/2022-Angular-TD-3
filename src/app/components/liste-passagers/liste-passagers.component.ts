64260034b4070903fdff90d354aa82c90aeea7cd
celestindeal <66480838+celestindeal@users.noreply.github.com>
2022-05-09 15:34:33 +0200

    toggle photo

--- src/app/components/liste-passagers/liste-passagers.component.ts
+++ src/app/components/liste-passagers/liste-passagers.component.ts
@@ -1,12 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
 import { Passager } from 'src/app/models/passager.model';
 import { Vol } from 'src/app/models/vol.model';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-liste-passagers',
   templateUrl: './liste-passagers.component.html',
   styleUrls: ['./liste-passagers.component.scss']
 })
-export class ListePassagersComponent {
+export class ListePassagersComponent   {
   @Input() vol! : Vol;
+  photos:boolean=false;
+
+  onChangePhoto(){
+    this.photos = !this.photos;
+    console.log(this.photos);
+  }
 }
